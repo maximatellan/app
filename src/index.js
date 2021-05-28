ad5c83e0508b3fe39b33696f5865a4465277b807
maximatellan <maximatellan@gmail.com>
2021-05-28 12:13:19 -0300

    Code

--- src/index.js
+++ src/index.js
@@ -5,10 +5,5 @@ import './css/style.css'
 logMessage('Its finished!!')
 
 
-function showValue(value){
-	document.getElementById("range").innerHTML=value;
-}
-
-
 if (module.hot)       // eslint-disable-line no-undef
   module.hot.accept() // eslint-disable-line no-undef
